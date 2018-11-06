b7ffbf2ee5ae68818135eb2359e3d5641cbfbceb
lcm <linchunmin1997@gmail.com>
2018-11-06 16:15:18 +0800

    Update predict.py

--- Single_device/predict.py
+++ Single_device/predict.py
@@ -2,17 +2,18 @@ from Data import get_static_frame_and_stacked_opt_flows
 from Two_stream_model import two_stream_model
 import numpy as np
 from keras import backend as K
+import time
 
 
 def main():
     test_data = get_static_frame_and_stacked_opt_flows()
     model = two_stream_model()
-
+    start = time.time()
     for i in range(50):
         result = model.predict([np.expand_dims(test_data[0], axis=0),
                                 np.expand_dims(test_data[1], axis=0)])
-        print(result)
-
+    end = time.time()
+    total_time = end - start
     K.clear_session()
 
 
